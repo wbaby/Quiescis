11b69986635ba1756429edcbce06ee7126d5f8ca
Nikait <aaanikiytgmail.com>
2020-09-14 21:51:15 +0300

    linux support

--- server/server/Utils.h
+++ server/server/Utils.h
@@ -2,8 +2,13 @@
  * Something functions
 */
 
+#ifndef UTILS_H
+#define UTILS_H
+
 // printing error
 void throw_error(const char* error_text);
 
 // checking existions of directory
 bool dirExists(const std::string& dirName_in);
+
+#endif // UTILS_H
